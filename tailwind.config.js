843785b6be72dc68ec3f80a550bdafb07b4237e0
mohamedagha <mohamedhamada3231@gmail.com>
2025-08-12 18:19:32 +0300

    firs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
